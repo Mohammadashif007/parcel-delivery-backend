fadddbd517f8e4e721f8437d60485fa1ce155d67
Mohammad Ashif <mohammadashif510@gmail.com>
2025-07-29 19:56:53 +0600

    higher order function implemented

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,9 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { NextFunction, Request, Response } from "express";
 import { UserServices } from "./user.service";
 import httpStatus from "http-status-codes";
+import { catchAsync } from "../../utils/catchAsync";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
+const createUser = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
         const userData = req.body;
         const result = await UserServices.createUserIntoDB(userData);
         res.status(httpStatus.CREATED).json({
@@ -11,23 +15,19 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
             message: "User created successfully",
             data: result,
         });
-    } catch (error) {
-        next(error);
     }
-};
+);
 
-const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
-    try {
+const getAllUsers = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
         const result = await UserServices.getAllUsersFromDB();
         res.status(httpStatus.CREATED).json({
             success: true,
             message: "User created successfully",
             data: result,
         });
-    } catch (error) {
-        next(error);
     }
-};
+);
 
 export const UserControllers = {
     createUser,
